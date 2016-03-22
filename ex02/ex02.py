3515358533e86bcfdd0e3cc6c8eeb2c560d882d6
CAD Client <CAD Client>
2016-03-22 12:06:43 +0900

    learn Calculation

--- ex02/ex02.py
+++ ex02/ex02.py
@@ -1,3 +1,10 @@
-print " I could have code I like this"
+# A comment, this is so you can read your program later
+# Anything after the # is isgonred by python
+
+print " I could have code I like this" # and the commnet is ignored
+
+# You can also use a commnet to "disable" or commnet out a piece of code:
+# print "This won't run."
+
 print " This will run"
-print " This won`t run"
+
---
+++ ex03/ex03.py
@@ -0,0 +1,25 @@
+print "I will now count my chickens:"
+
+print "Hens", 25 + 30 / 6
+print "Rosster", 100- 25 * 3 % 4
+
+print " Now I will connt the eggs:"
+
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+print "Is it true that 3 + 2 < 5 -7 ?"
+
+print  3 + 2 < 5 - 7
+
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7?", 5 - 7
+
+print  "Oh, that`s why it`s Flase."
+
+print  "How about some more."
+
+print "Is is greater?", 5>-2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
+
+print 13.00 + 5.00 / 4.00
