fca01d1f988a19ace75dfe4d07e8e23ba7c4030f
CAD Client <CAD Client>
2016-03-29 11:59:25 +0900

    Study Drill

--- ex05/ex05.py
+++ ex05/ex05.py
@@ -1,20 +1,32 @@
 # -*- coding: utf-8 -*- #한글패치
 # -*- coding: cp949 -*-
-my_name = 'JY.Park박'
-my_age = 23 # not a lie
-my_height = 174 # cm
-my_weight = 65 # kg
-my_eyes = 'Black'
-my_teeth = 'White'
-my_hair = 'Black and White'
+print " 한글패치"
+name = 'JY.Park박'
+age = 23 # not a lie
+height_cm = 174.2 # cm
+weight_kg = 65 # kg
+eyes = 'Black'
+teeth = 'White'
+hair = 'Black and White'
+cm_to_inch = 1.0/ 2.54
+height_inch = height_cm * cm_to_inch
 
-print "Let's talk about %s." % my_name
-print "He's %d cm tall." % my_height
-print "He's %d kg heavy." % my_weight
+print "Let's talk about %s." % name
+print "He's %g cm tall." % height_cm
+print "He`s %g inch tall" % height_inch
+print "He's %d kg heavy." % weight_kg
 print "Actually that's not too heavy."
-print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
-print "His teeth are usually %s depending on the coffee." % my_teeth
+print "He's got %s eyes and %s hair." % (eyes, hair)
+print "His teeth are usually %s depending on the coffee." % teeth
 
 # this line is tricky, try to get it exactly right
 print "If I add %d, %d, and %d I get %d." % (
-    my_age, my_height, my_weight, my_age + my_height + my_weight)
+    age, height_cm, weight_kg, age + height_cm + weight_kg)
+#Study drill
+print "hey your`s name %s? " % name
+print "hey your`s name"
+#%는 대입 비슷한것이라고 보면 된다.
+#%r은 절대로 출력되지 않는다.
+#%d 는 숫자열이지만 소숫자리는 버린다 %g는 반면에 소숫자리까지 표시
+#%s 는 문자열
+
