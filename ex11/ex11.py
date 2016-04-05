aba33fb913de3312d6ab47d5dc4172acb7c3b007
CAD Client <CAD Client>
2016-04-05 12:18:17 +0900

    ex11 raw_put 사용법

---
+++ ex11/ex11.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*- #한글패치
+# -*- coding: cp949 -*-
+print " 한글패치"
+
+print "How old are you?",
+age = raw_input()
+print "How tall are you?",
+height = raw_input()
+print "How much do you weigh?",
+weight = raw_input()
+
+print "So, you're %r old, %r tall and %r heavy." % (
+    age, height, weight)
