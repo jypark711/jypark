75bc48107ce42c2ddc8ba720cf080aea16d96bd5
CAD Client <CAD Client>
2016-04-05 11:03:53 +0900

    8,9 완료

---
+++ ex09/ex09.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*- #한글패치
+# -*- coding: cp949 -*-
+print " 한글패치"
+
+# Here`s some new strang stuff, remeber type it exactly
+
+days ="Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+#\는 원화기호지만 BackSlash기능을 한다. BackSlash는 다음열에 나눠서 정렬하는 역할을 한다.
+print "Here are the days: ", days
+print "Here are the months:",months
+
+print """
+  There`s something going on here.
+   with the three double-quotes.
+   we`ll be able to type as much as we like.
+   Even 4 lines if we want, or 5. or 6
+"""
+#""""      "는 backslash와 같은 기능을 한다.  
