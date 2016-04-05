72f795892a826ee0ba57c1e9cb4e38fafdd0a4b1
CAD Client <CAD Client>
2016-04-05 11:55:31 +0900

    '로 인해 다른문자열만 ""출력

--- ex08/ex08.py
+++ ex08/ex08.py
@@ -5,9 +5,8 @@ formatter = "%r %r %r %r"
 print formatter % (1, 2, 3, 4)
 print formatter % ("one", "two", "three", "four")
 print formatter % (formatter, formatter, formatter, formatter)
-print formatter %  (
-                    "I had this thing.",
+print formatter %  ( "I had this thing.",
                      "That you could type up right.",
-                     "But it didn`t sing.",
+                     "But it didn't sing.",
                      "So I said goodnight.")
-
+# ' 로 인해 "But it didn't sing."이 문장만 ""로 출력   " '안의 다른 문자열로 인식하기 때문에
