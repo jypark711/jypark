5c85b0776d8e9c6386e5a77ceb8a8ac03ed00ba4
CAD Client <CAD Client>
2016-04-12 11:44:22 +0900

    ex11과 비교해보자

---
+++ ex12/ex12.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*- #한글패치
+# -*- coding: cp949 -*-
+print " 한글패치"
+#파이선3 에서는 그냥 input으로
+age = raw_input("How old are you")
+height = raw_input("How tall are you?")
+weight = raw_input("How much do you weigh")
+
+print "so, you're %r old, %r tall and %r heavy. " % (age, height, weight)
+#%r을 이용하여 마지막에 출력을 한다.
+
