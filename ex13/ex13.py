d3e344e875354a89dc3321dbd093422069d74353
CAD Client <CAD Client>
2016-04-12 12:22:41 +0900

    예제 13 끝 나머지는 아직

---
+++ ex13/ex13.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*- #한글패치
+# -*- coding: cp949 -*-
+
+from sys import argv
+
+print ("argv ="  +str(argv))
+script, first, second, third = argv
+
+print ("The script is called: %s" % script)
+print ("Your first variable is: %s" % first)
+print ("Your second variable is: %s" % second)
+print ("Your third variable is: %s" % third)
+
+# Edit Configuration 선택
+#[Configuration] tab 두번째 Script parameters 변경
---
+++ ex15/ex15.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*- #한글패치
+# -*- coding: cp949 -*-
+print " 한글패치"
